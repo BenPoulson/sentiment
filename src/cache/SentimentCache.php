2d218484171a558ec09fd7ce8510550eced6c315
Ben Poulson <ben@3sidedcube.com>
2015-09-11 21:29:42 +0100

    Remove superfluous methods

--- src/cache/SentimentCache.php
+++ src/cache/SentimentCache.php
@@ -5,6 +5,5 @@ namespace BenPoulson\Sentiment\Cache;
 use Camspiers\StatisticalClassifier\DataSource\DataArray;
 
 abstract class SentimentCache extends DataArray {
-    public abstract function read();
-    public abstract function write();
+
 }
